6be417251d97b5cbb7988d4f748ddd3e61056a00
sahil <preetsahil289@gmail.com>
2023-10-15 16:25:00 +0530

    Make Entire Card Area Clickable

--- components/movies-list.tsx
+++ components/movies-list.tsx
@@ -1,15 +1,14 @@
 import Link from 'next/link'
 import { Titles } from '~/lib/xata.codegen'
-import { Rating } from './ratings'
 
 const MovieCard = ({ data }: { data: Titles }) => {
   const { primaryTitle, genres, coverUrl, summary, averageRating, id } = data
 
   return (
     <li className="flex w-full h-full max-w-sm mx-auto overflow-hidden transition duration-500 ease-in-out shadow-lg bg-slate-950 rounded-xl hover:-translate-y-2 hover:shadow-2xl relative text-white after:absolute after:inset-0 after:bg-gradient-to-t after:from-black after:via-slate-900 after:to-transparent focus-within:outline focus-within:outline-pink-600">
-      <div className="relative z-10 px-6 pb-10 group">
-        <header className="flex flex-col inner pb-6">
-          <Link href={`/movie/${id}`} className="block pt-40 outline-none">
+      <Link href={`/movie/${id}`} className="block pt-40 outline-none">
+        <div className="relative z-10 px-6 pb-10 group">
+          <header className="flex flex-col inner pb-6">
             <h2 className="text-2xl min-h-[4rem] font-bold text-white flex flex-col justify-center">
               {primaryTitle}
             </h2>
@@ -24,36 +23,33 @@ const MovieCard = ({ data }: { data: Titles }) => {
                 </li>
               ))}
             </ul>
-          </Link>
-        </header>
-        <div className="pb-8">
-          {averageRating ? <Rating value={averageRating} title={id} /> : null}
+          </header>
+          {summary && (
+            <p className="pt-2 pb-6 text-sm text-slate-100">
+              {summary.length > 260 ? summary.substring(0, 250) + '…' : summary}
+            </p>
+          )}
         </div>
-        {summary && (
-          <p className="pt-2 pb-6 text-sm text-slate-100">
-            {summary.length > 260 ? summary.substring(0, 250) + '…' : summary}
-          </p>
-        )}
-      </div>
 
-      {coverUrl?.startsWith('http') && !coverUrl?.endsWith('null') ? (
-        <picture>
-          <source srcSet={coverUrl} type="image/webp" />
-          <img
-            src={coverUrl}
-            alt={`Poster for "${primaryTitle}"`}
-            className="absolute inset-0 w-full transform -translate-y-4 grayscale-[0.7]"
-          />
-        </picture>
-      ) : (
-        <picture>
-          <img
-            src="/placeholder.jpg"
-            alt="Placeholder image"
-            className="absolute inset-0 w-full transform -translate-y-4"
-          />
-        </picture>
-      )}
+        {coverUrl?.startsWith('http') && !coverUrl?.endsWith('null') ? (
+          <picture>
+            <source srcSet={coverUrl} type="image/webp" />
+            <img
+              src={coverUrl}
+              alt={`Poster for "${primaryTitle}"`}
+              className="absolute inset-0 w-full transform -translate-y-4 grayscale-[0.7]"
+            />
+          </picture>
+        ) : (
+          <picture>
+            <img
+              src="/placeholder.jpg"
+              alt="Placeholder image"
+              className="absolute inset-0 w-full transform -translate-y-4"
+            />
+          </picture>
+        )}
+      </Link>
     </li>
   )
 }
